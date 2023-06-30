801632c6b7458671d200230029d9fe5230cf7a5d
TAPAN-Agrawal <taapanagrawal2002@gmail.com>
2023-06-30 16:34:05 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+
+//actions
+
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// REDUCERS
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+//store
+
+const store = createStore(counter);
+
+//dispatch
+
+store.dispatch(increment());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
